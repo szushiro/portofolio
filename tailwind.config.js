c5797a1d1aab0d5f6d0b782c76c4e45c6eca1741
Agas Pratama <165492024+szushiro@users.noreply.github.com>
2024-11-30 23:32:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1f2937",
+        secondary: "#facc15"
+      } 
+    },
+  },
+  plugins: [],
+}
